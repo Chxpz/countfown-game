be95aa2d6e5e5950a94c646f7ad83d5a17f8f005
chxpz <rpcweb3@gmail.com>
2024-02-07 08:50:47 -0300

    add createPortal for modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(
   ({ targetTime, remainingTime, onReset }, ref) => {
@@ -17,7 +18,7 @@ const ResultModal = forwardRef(
       };
     });
 
-    return (
+    return createPortal(
       <dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>Time's up!</h2>}
         {userWon && (
@@ -34,7 +35,8 @@ const ResultModal = forwardRef(
         <form method="dialog" onSubmit={onReset}>
           <button>Close</button>
         </form>
-      </dialog>
+      </dialog>,
+      document.getElementById("modal")
     );
   }
 );
